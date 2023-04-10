ae2c6178f4ce02754a49ab1612b19996c9fbc6d4
lawrencejews <lawrencejews1@gmail.com>
2023-04-10 20:38:37 +0900

    complted dashboard navbar

---
+++ client/src/App.tsx
@@ -0,0 +1,36 @@
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
+
+// There 
+function App() {
+
+  // Get theme and expanded-theme class for css
+  const theme = useMemo(() => createTheme(themeSettings), [])
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<div>predictions page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+
+    </div>
+  )
+}
+
+
+export default App
---
+++ client/src/components/FlexBetween.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import styled from "@emotion/styled";
+
+const FlextBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center"
+})
+
+export default FlextBetween;
